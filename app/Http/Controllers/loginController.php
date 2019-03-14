4ec30504aad6c0aea799e5357f435e70813964b5
陳佳妤 <athena72225yahoo.com.tw@39-243.wireless-city.scu.edu.tw>
2019-03-14 15:53:45 +0800

    update database

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -67,9 +67,18 @@ class loginController extends Controller
          $user->id_token= $id_token;
          $user->save();
          $credentials = request(['citizen_id', 'password']);
-         $token = auth('api')->attempt($credentials);
+        try 
+        { $token = auth('api')->attempt($credentials);
 
          $this->storerem_token($id_token,$token);
+       }
+       catch (Exception $e) {
+        
+       return  response()->json([
+            'error' => $e->getMessage()
+        
+]);
+       }
          	
          return response()->json([
             'id_token' => $id_token,
