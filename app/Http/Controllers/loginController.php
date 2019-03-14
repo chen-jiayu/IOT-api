a8220f4981bf0b125d714d5ee282983da4d7cf0b
陳佳妤 <athena72225yahoo.com.tw@39-243.wireless-city.scu.edu.tw>
2019-03-14 15:32:25 +0800

    update database

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -58,6 +58,8 @@ class loginController extends Controller
          
       if(empty(DB::table('users')->where('citizen_id', '=',$request->input('citizen_id') )->value('citizen_id'))==true){
          $id_token=bcrypt($request->input('citizen_id'));
+         $credentials = request(['citizen_id', 'password']);
+         $token = auth('api')->attempt($credentials);
          $user = new user();
          $user->user_name= $request->input('user_name');
          $user->mobile= $request->input('mobile');
@@ -65,11 +67,12 @@ class loginController extends Controller
          $user->citizen_id= $request->input('citizen_id');
          $user->password= bcrypt($request->input('password'));
          $user->id_token= $id_token;
+         $user->remeber_token=$token;
          $user->save();
-         $credentials = request(['citizen_id', 'password']);
-         $token = auth('api')->attempt($credentials);
+        // $credentials = request(['citizen_id', 'password']);
+         //$token = auth('api')->attempt($credentials);
 
-         $this->storerem_token($id_token,$token);
+         //$this->storerem_token($id_token,$token);
          	
          return response()->json([
             'id_token' => $id_token,
