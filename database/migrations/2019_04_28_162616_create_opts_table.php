9240f1b75236e8bcfbeb7ce793873bcf3e3c71b5
陳佳妤 <athena72225yahoo.com.tw@39-132.wireless-city.scu.edu.tw>
2019-04-30 16:12:15 +0800

    update database

--- database/migrations/2019_04_28_162616_create_opts_table.php
+++ database/migrations/2019_04_28_162616_create_opts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOptionsTable extends Migration
+class CreateOptsTable extends Migration
 {
     /**
      * Run the migrations.
